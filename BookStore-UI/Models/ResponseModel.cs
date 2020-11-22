68cabcfd735282eaeaf59f3ea6749e9aa0cf1fde
kostasgkor <kgkorit@gmail.com>
2020-11-22 02:32:00 +0200

    Added Blazor Project. Completed User Authentication tasks in both API and Blazor Application.

---
+++ BookStore-UI/Models/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Models
+{
+    public class ResponseModel
+    {
+        public bool Success { get; set; }
+        public bool Error { get; set; }
+        public bool Content { get; set; }
+    }
+}
