e2ac94e29b5f5d2f14a601960fcbe78b9237b388
kostasgkor <kgkorit@gmail.com>
2020-11-25 20:00:07 +0200

    Added Book CRUD Operations, Fixed Book API DTO issues and added Toastnotifications

--- BookStore-UI/Startup.cs
+++ BookStore-UI/Startup.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Blazored.Toast;
 using BookStore_UI.Contracts;
 using BookStore_UI.Data;
 using BookStore_UI.Providers;
@@ -35,12 +36,15 @@ namespace BookStore_UI
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddBlazoredLocalStorage();
+            services.AddBlazoredToast();
             services.AddHttpClient();
             services.AddScoped<ApiAuthenticationStateProvider>();
             services.AddScoped<AuthenticationStateProvider>(p => p.GetRequiredService<ApiAuthenticationStateProvider>());
             services.AddScoped<JwtSecurityTokenHandler>();
             services.AddTransient<IAuthenticationRepository, AuthenticationRepository>();
             services.AddTransient<IAuthorRepository, AuthorRepository > ();
+            services.AddTransient<IBookRepository, BookRepository > ();
+            services.AddTransient<IFileUpload, FileUpload > ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
